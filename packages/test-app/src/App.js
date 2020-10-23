daa7702d5b3fa37c13897f18ab8e50866e54a17e
c3dr0x <cedric.boulet-kessler@dreamquark.com>
2020-10-23 14:48:57 +0200

    feat: init react-create-app

---
+++ packages/test-app/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { TopAppBar, TopAppBarRow, TopAppBarSection, TopAppBarTitle } from "@rmwc/top-app-bar";
+
+function App() {
+    return <div className="App"></div>;
+}
+
+export default App;
